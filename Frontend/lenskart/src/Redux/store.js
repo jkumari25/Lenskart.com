c7fea528942c2296afea3e4c6a220157d817c435
jkumari25 <jagriti.sj15@gmail.com>
2023-02-21 22:28:20 +0530

    Basic Folder

---
+++ Frontend/lenskart/src/Redux/store.js
@@ -0,0 +1,6 @@
+import {legacy_createStore} from "redux";
+import thunk from "redux-thunk";
+
+const store= legacy_createStore();
+
+export {store};
---
+++ Frontend/lenskart/src/Routes/AllRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Routes,Route} from "react-router-dom";
+
+const AllRoutes = () => {
+  return (
+   <Routes>
+    
+   </Routes>
+  )
+}
+
+export default AllRoutes
