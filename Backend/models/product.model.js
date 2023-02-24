1dc232af58fbdd3c9c4c8d807778dba5e534d883
SafwanFaiz <safwanfaiz8@gmail.com>
2023-02-24 20:43:11 +0530

    final backend

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -1,21 +1,35 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-    main_image: { type: String, required: true },
-  product_details_box: { type: Number, required: true },
-  rating_count:  { type: Number, required: true },
-  product_details : { type: String, required: true },
-  size_details: { type: String, required: true },
-  price_text: { type: Number, required: true },
-  prev_price: { type: Number, required: true },
-  color: { type: String, required: true },
-  offer_box: { type: String, required: true },
-  img1: { type: String, required: true },
-  img2: { type: String, required: true },
-  img3: { type: String, required: true },
-  img4: { type: String, required: true }
-});
+  product_image: { type: String, required: true },
+  rating_value: { type: String, required: true },
+  rating_count:  { type: String, required: true },
+  product_name : { type: String, required: true },
+  size: { type: String, required: true },
+  tags: { type: String, required: true },
+  product_price: { type: Number, required: true },
+  strike_through: { type: String, required: true },
+  product_offer: { type: String, required: true },
+  category: { type: String, required: true },
+  id: { type: Number, required: true },
+},{versionKey:false},);
 
 const ProductModel = mongoose.model("product", productSchema);
 
 module.exports = { ProductModel };
+
+
+
+
+
+// product_image: ""
+// rating_value: "5.3"
+// rating_count:  "12"
+// product_name : "safwan"
+// size: "medium"
+// tags: 32
+// product_price: 23,
+// strike_through: "men cassual"
+// product_offer: "999 only"
+// category: "men"
+// id: 1,
--- Backend/models/signup.model.js
+++ Backend/models/signup.model.js
@@ -4,7 +4,7 @@ const signupSchema = mongoose.Schema({
   name: String,
   email: String,
   password: String,
-});
+},{versionKey:false},);
 
 const signupModel = mongoose.model("signup", signupSchema);
 
